b85d1f89cc9aa5cc60e31d94cd6b409862961f11
Sarah Murray <sarahmurraydev@gmail.com>
2022-04-29 14:53:03 -0400

    set up fragment and nav graph

--- app/src/main/java/com/example/mobiustodoapp/MainActivity.kt
+++ app/src/main/java/com/example/mobiustodoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mobiustodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set up navigation component:
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+        // note: do this to insure you aren't recreating the nav graph every time app undergoes config changes
+        if (savedInstanceState == null) navController.setGraph(R.navigation.nav_graph)
     }
 }
